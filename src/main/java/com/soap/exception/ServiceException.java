74669326afdaf459134c40e8f0067e8d9aae3587
Addin Munawwar <moonawar@protonmail.com>
2023-10-27 00:49:28 +0700

    feat: add service exception

---
+++ src/main/java/com/soap/exception/ServiceException.java
@@ -0,0 +1,12 @@
+package com.soap.exception;
+import javax.xml.ws.WebFault;
+
+@WebFault(name = "ServiceException")
+public class ServiceException extends Exception{
+    public int code = 400;
+    
+    public ServiceException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
