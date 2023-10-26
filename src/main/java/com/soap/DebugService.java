d68d03ef8e8a8e35c09e881c2c56562590b9590b
Addin Munawwar <moonawar@protonmail.com>
2023-10-26 16:40:20 +0700

    style: change folder dirname

--- src/main/java/com/soap/DebugService.java
+++ src/main/java/com/soap/DebugService.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.soap;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
--- src/main/java/com/soap/Main.java
+++ src/main/java/com/soap/Main.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.soap;
 
 import javax.xml.ws.Endpoint;
 
