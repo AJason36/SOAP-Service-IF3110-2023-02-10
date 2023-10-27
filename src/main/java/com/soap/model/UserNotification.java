b6b16775316863a1ca4a3ead32b8003e1fefbaef
Addin Munawwar <moonawar@protonmail.com>
2023-10-27 15:25:43 +0700

    feat: JPA for user notification

--- src/main/java/com/soap/model/UserNotification.java
+++ src/main/java/com/soap/model/UserNotification.java
@@ -2,6 +2,15 @@ package com.soap.model;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.soap.model.enums.NotificationStatus;
 
 import lombok.AllArgsConstructor;
@@ -11,10 +20,24 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
+@Table(name = "user_notification")
 public class UserNotification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
     private int notificationId;  
-    private User user;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "message")
     private String message;
+
+    @Column(name = "created_at")
     private Timestamp createdAt;
-    private NotificationStatus status;  
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private NotificationStatus status;
 }
