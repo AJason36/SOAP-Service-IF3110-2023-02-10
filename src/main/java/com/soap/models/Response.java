f1ec3e14aab2b8c46d6d2a18cf6e5a3178c611ef
Addin Munawwar <moonawar@protonmail.com>
2023-11-14 18:43:00 +0700

    feat: remodel soap response class (2)

--- src/main/java/com/soap/models/Response.java
+++ src/main/java/com/soap/models/Response.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Response<T> {
-    private ResponseCode code;
+    private int code;
     private String message;
     private T data;
 }
--- src/main/java/com/soap/models/ResponseCode.java
+++ src/main/java/com/soap/models/ResponseCode.java
@@ -1,18 +1,8 @@
 package com.soap.models;
 
-public enum ResponseCode {
-    SUCCESS (200),
-    SERVER_ERROR (500),
-    BAD_REQUEST (400),
-    NOT_FOUND (404);
-
-    private final int code;
-
-    ResponseCode(int code) {
-        this.code = code;
-    }
-
-    public int getCode() {
-        return code;
-    }
+public class ResponseCode {
+    public final static int SUCCESS = 200;
+    public final static int SERVER_ERROR = 500;
+    public final static int BAD_REQUEST = 400;
+    public final static int NOT_FOUND = 404;
 }
